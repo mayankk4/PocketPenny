67d89c9f1e6ae13b8d3fb88b94a1a1ec2b85f056
mayankk4@gmail.com <mayankk4@gmail.com@8a4d664d-70e0-44f4-806d-ec41310e5d3c>
2011-02-12 08:38:48 +0000

    main flow

--- PocketPenny/main.cpp
+++ PocketPenny/main.cpp
@@ -6,7 +6,81 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    //ProfileInfo p = new ProfileInfo("gaurav", 10000,8000);
-    cout<<" hie manku "<<endl;
-    return a.exec();
+
+
+    cout << "App starting ... "<<endl;
+
+    // create database tables
+    // connect to database
+    // create user profile
+
+    int option;
+
+    while(1){
+    cout << "Enter 1 to add expense"  << endl
+         << "Enter 2 to view all expenses" << endl
+         << "Enter 3 to edit an expense" << endl
+
+         << "Enter 4 to add catagory" << endl
+         << "Enter 5 to view all catagories" << endl
+         << "Enter 6 to edit catagory" << endl
+
+         << "Enter 7 to view user profile" << endl
+         << "Enter 8 to edit user profile" << endl
+
+         << "Enter 9 to view records of past months" << endl
+         << "Enter 0 to exit" << endl;
+
+    cin >> option; // edit includes edit and delete
+
+    switch(option){
+
+        case 1 :
+
+            break;
+
+        case 2 :
+
+            break;
+
+        case 3 :
+
+            break;
+
+        case 4 :
+
+            break;
+
+        case 5 :
+
+            break;
+
+        case 6 :
+
+            break;
+
+        case 7 :
+
+            break;
+
+        case 8 :
+
+            break;
+
+        case 9 :
+
+            break;
+
+        case 0 :
+
+            break;
+
+        default :
+
+            break;
+
+    }
+
+  }
+  return a.exec();
 }
